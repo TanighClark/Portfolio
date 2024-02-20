ae7ded36e1e724e9a74b70450b4f8b36bdbdc1d6
TanighClark <137099207+TanighClark@users.noreply.github.com>
2024-02-19 19:27:15 -0500

    2/19

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import About from './Pages/About';
 import Contact from './Pages/Contact';
-import Home from './Pages/Home'
-import Portfolio from './Pages/Portfolio'
+import Home from './Pages/Home';
+import Portfolio from './Pages/Portfolio';
+import { Analytics } from '@vercel/analytics/react';
 
-
-
-function App() {
+function App({ Component, pageProps }) {
   return (
     <BrowserRouter>
-      
+      <>
+        <Component {...pageProps} />
+        <Analytics />
+      </>
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/Pages/Portfolio' element={<Portfolio/>}/>
-        <Route path='/Pages/About' element={<About/>}/>
-        <Route path='/Pages/Contact' element={<Contact/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/Pages/Portfolio" element={<Portfolio />} />
+        <Route path="/Pages/About" element={<About />} />
+        <Route path="/Pages/Contact" element={<Contact />} />
       </Routes>
-
     </BrowserRouter>
   );
 }
